78896b54665f92003e8ea9408af80eb3e77b9d5e
AshKadlag <aishwaryakadlag@gmail.com>
2017-03-18 11:28:04 +0530

    New design

--- gui_final.py
+++ gui_final.py
@@ -4,6 +4,7 @@ import sys
 from PyQt4 import QtCore, QtGui
 import webbrowser
 import urllib
+'''
 import cv2
 import sys
 from resnet50 import ResNet50
@@ -15,6 +16,7 @@ import time
 import cv2
 exit = 0
 model = ResNet50(weights='imagenet')
+'''
 
 
 def start_application():
@@ -124,70 +126,123 @@ class MainMenu(QtGui.QWidget):
 		super(MainMenu, self).__init__()
 		
 		self.createMenu()
+
+		buttonBox = QtGui.QDialogButtonBox(QtGui.QDialogButtonBox.Ok | QtGui.QDialogButtonBox.Cancel)
+		buttonBox.accepted.connect(start_application)
+		buttonBox.rejected.connect(self.close_application)
+		buttonBox.setStyleSheet("QPushButton { color: rgb(50,80,109);\
+            font-size: 12pt;border: ;background-color: rgb(255, 255, 255);\
+            border-style: outset;border-width: 2px;border-radius: 5px;\
+            border-color: white;padding: 4px;}");
 		
 		btn1 = QtGui.QPushButton("Label a Image", self)
 		btn1.clicked.connect(self.imageLabel)
-		btn1.setStyleSheet("QPushButton { color: rgb(255, 255, 255);\
+		btn1.setIcon(QtGui.QIcon('camera2.png'))
+		btn1.setIconSize(QtCore.QSize(130,130))
+		btn1.setGeometry(QtCore.QRect(1030, 500, 161, 61))
+		btn1.setFixedWidth(300)
+		btn1.setFixedHeight(170)
+		btn1.setStyleSheet("QPushButton { color: rgb(0,0,0);\
             font-size: 16pt;border: ;background-color: rgb(10,180,255);\
             border-style: outset;border-width: 2px;border-radius: 15px;\
             border-color: white;padding: 4px;} QPushButton: ");
+		'''
 		btn1.resize(250,150)
-		btn1.move(150,100)
+		btn1.move(300,200)
+		'''
 
 		btn2 = QtGui.QPushButton("Label a Video", self)
 		btn2.clicked.connect(self.imageLabel)
-		btn2.setStyleSheet("QPushButton { color: rgb(255, 255, 255);\
+		btn2.setIcon(QtGui.QIcon('video.png'))
+		btn2.setIconSize(QtCore.QSize(130,130))
+		btn2.setGeometry(QtCore.QRect(1030, 500, 161, 61))
+		btn2.setFixedWidth(300)
+		btn2.setFixedHeight(170)
+		btn2.setStyleSheet("QPushButton { color: rgb(0,0,0);\
             font-size: 16pt;border: ;background-color: rgb(10,180,255);\
             border-style: outset;border-width: 2px;border-radius: 15px;\
             border-color: white;padding: 4px;}");
+		'''
 		btn2.resize(250,150)
-		btn2.move(630,100)
+		btn2.move(1000,200)
+		'''
 
 		btn3 = QtGui.QPushButton("Extract\nParameters", self)
 		btn3.clicked.connect(self.imageLabel)
-		btn3.setStyleSheet("QPushButton { color: rgb(255, 255, 255);\
+		btn3.setIcon(QtGui.QIcon('airplane-shape.png'))
+		btn3.setIconSize(QtCore.QSize(130,130))
+		btn3.setGeometry(QtCore.QRect(1030, 500, 161, 61))
+		btn3.setFixedWidth(300)
+		btn3.setFixedHeight(170)
+		btn3.setStyleSheet("QPushButton { color: rgb(0,0,0);\
             font-size: 16pt;border: ;background-color: rgb(10,180,255);\
             border-style: outset;border-width: 2px;border-radius: 15px;\
             border-color: white;padding: 4px;}");
+		'''
 		btn3.resize(250,150)
-		btn3.move(1100,100)
+		btn3.move(200,500)
+		'''
 
 		btn4 = QtGui.QPushButton("Label1", self)
 		btn4.clicked.connect(self.imageLabel)
-		btn4.setStyleSheet("QPushButton { color: rgb(255, 255, 255);\
+		btn4.setIcon(QtGui.QIcon('search.png'))
+		btn4.setIconSize(QtCore.QSize(130,130))
+		btn4.setGeometry(QtCore.QRect(1030, 500, 161, 61))
+		btn4.setFixedWidth(300)
+		btn4.setFixedHeight(170)
+		btn4.setStyleSheet("QPushButton { color: rgb(0,0,0);\
             font-size: 16pt;border: ;background-color: rgb(10,180,255);\
             border-style: outset;border-width: 2px;border-radius: 15px;\
             border-color: white;padding: 4px;}");
+		'''
 		btn4.resize(250,150)
-		btn4.move(400,500)
+		btn4.move(600,500)
+		'''
 
 		btn5 = QtGui.QPushButton("Label2", self)
 		btn5.clicked.connect(self.imageLabel)
-		btn5.setStyleSheet("QPushButton { color: rgb(255, 255, 255);\
+		btn5.setIcon(QtGui.QIcon('smartphone.png'))
+		btn5.setIconSize(QtCore.QSize(130,130))
+		btn5.setGeometry(QtCore.QRect(1030, 500, 161, 61))
+		btn5.setFixedWidth(300)
+		btn5.setFixedHeight(170)
+		btn5.setStyleSheet("QPushButton { color: rgb(0,0,0);\
             font-size: 16pt;border: ;background-color: rgb(10,180,255);\
             border-style: outset;border-width: 2px;border-radius: 15px;\
             border-color: white;padding: 4px;}");
+		'''
 		btn5.resize(250,150)
-		btn5.move(900,500)
+		btn5.move(1100,500)
+		'''
 
-		btn6 = QtGui.QPushButton("Quit", self)
-		btn6.clicked.connect(self.close_application)
-		btn6.setStyleSheet("QPushButton { color: rgb(255, 255, 255);\
-            font-size: 16pt;border: ;background-color: rgb(204,78,92);\
-            border-style: outset;border-width: 2px;border-radius: 15px;\
-            border-color: white;padding: 4px;}");
-		btn6.resize(btn6.minimumSizeHint())
-		btn6.move(1400,800)
+		Hlayout1 = QtGui.QHBoxLayout()
+		Hlayout1.addWidget(btn1)
+		Hlayout1.addWidget(btn2)
+
+		Hlayout2 = QtGui.QHBoxLayout()
+		Hlayout2.addWidget(btn3)
+		Hlayout2.addWidget(btn4)
+		Hlayout2.addWidget(btn5)
 
 		mainLayout = QtGui.QVBoxLayout()
+		mainLayout.addLayout(Hlayout1)
+		mainLayout.addLayout(Hlayout2)
+		mainLayout.addWidget(buttonBox)
 		mainLayout.setMenuBar(self.menuBar)
 		
 		self.setLayout(mainLayout)
 		
-		self.setStyleSheet("background-color: rgb(10,10,10);")
-		#self.setStyleSheet("background-image: url(Network.jpg);")
+		#self.setStyleSheet("background-color: rgb(10,10,10);")
+		self.setStyleSheet("background-image: url('background10.jpg');")
 		#btn1.setStyleSheet(" { background-color: rgb(58,83,155); }")
 		
+		p = QtGui.QPalette()
+		brush = QtGui.QBrush(QtCore.Qt.white,QtGui.QPixmap('background2.jpg'))
+		p.setBrush(QtGui.QPalette.Active,QtGui.QPalette.Window,brush)
+		p.setBrush(QtGui.QPalette.Inactive,QtGui.QPalette.Window,brush)
+		p.setBrush(QtGui.QPalette.Disabled,QtGui.QPalette.Window,brush)
+		self.setPalette(p)
+
 		self.setGeometry(1, 1, 2000, 1800)
 		self.setWindowTitle("Visual Recognition using CNN")
 		self.show()
@@ -260,10 +315,6 @@ class LabelImage(QtGui.QWidget):
 		buttonBox = QtGui.QDialogButtonBox(QtGui.QDialogButtonBox.Ok | QtGui.QDialogButtonBox.Cancel)
 		buttonBox.accepted.connect(start_application)
 		buttonBox.rejected.connect(self.close_application)
-		buttonBox.setStyleSheet("QPushButton { color: rgb(255, 255, 255);\
-            font-size: 12pt;border: ;background-color: rgb(50,80,109);\
-            border-style: outset;border-width: 2px;border-radius: 5px;\
-            border-color: white;padding: 4px;}");
 
 		mainLayout = QtGui.QVBoxLayout()
 		mainLayout.setMenuBar(self.menuBar)
@@ -273,9 +324,18 @@ class LabelImage(QtGui.QWidget):
 		
 		self.setLayout(mainLayout)
 		
-		self.setStyleSheet("background-color: rgb(255,255,255);")
-		self.setGeometry(1, 1, 350, 500)
-		self.move(650,200)
+		p = QtGui.QPalette()
+		brush = QtGui.QBrush(QtCore.Qt.white,QtGui.QPixmap('background22.jpg'))
+		p.setBrush(QtGui.QPalette.Active,QtGui.QPalette.Window,brush)
+		p.setBrush(QtGui.QPalette.Inactive,QtGui.QPalette.Window,brush)
+		p.setBrush(QtGui.QPalette.Disabled,QtGui.QPalette.Window,brush)
+		self.setPalette(p)
+
+		
+		#self.setStyleSheet("background-image: url('background10.jpg');")
+		#self.setStyleSheet("background-color: rgb(255,255,255);")
+		self.setGeometry(1, 1, 2000,1800)
+		self.move(500,200)
 		self.setWindowTitle("Visual Recognition using CNN")
 		self.show()
 
@@ -305,61 +365,77 @@ class LabelImage(QtGui.QWidget):
 		self.horizontalGroupBox = QtGui.QGroupBox()
 		layout = QtGui.QHBoxLayout()
 		
+		title = QtGui.QLabel("Select Image Source:")
+		title.setStyleSheet("QLabel {color: rgb(0,0,0); font-size: 18pt;}")
+		title.setFixedHeight(50)
+		
 		btn1 = QtGui.QPushButton("Browse", self)
-		image1 = btn1.clicked.connect(file_open)
-		btn1.setStyleSheet("QPushButton { color: rgb(255, 255, 255);\
-            font-size: 16pt;border: ;background-color: rgb(50,80,109);\
+		btn1.clicked.connect(file_open)
+		btn1.setFixedWidth(300)
+		btn1.setFixedHeight(100)
+		btn1.setStyleSheet("QPushButton { color: rgb(0,0,0);\
+            font-size: 18pt;border: ;background-image: url('background1.jpg');\
             border-style: outset;border-width: 2px;border-radius: 10px;\
             border-color: white;padding: 10px;}");
 		btn1.resize(btn1.minimumSizeHint())
 		btn1.move(4,10)
 		
 		btn2 = QtGui.QPushButton("Camera", self)
-		image = btn2.clicked.connect(camera_open)
-		btn2.setStyleSheet("QPushButton { color: rgb(255, 255, 255);\
-            font-size: 16pt;border: ;background-color: rgb(50,80,109);\
+		btn2.clicked.connect(camera_open)
+		btn2.setFixedWidth(300)
+		btn2.setFixedHeight(100)
+		btn2.setStyleSheet("QPushButton { color: rgb(0,0,0);\
+            font-size: 18pt;border: ;background-image: url('background1.jpg');\
             border-style: outset;border-width: 2px;border-radius: 10px;\
             border-color: white;padding: 10px;}");
 		btn2.resize(btn2.minimumSizeHint())
 		btn2.move(4,50)
 
 		btn3 = QtGui.QPushButton("Search", self)
-		image = btn3.clicked.connect(chrome_open)
-		btn3.setStyleSheet("QPushButton { color: rgb(255, 255, 255);\
-            font-size: 16pt;border: ;background-color: rgb(50,80,109);\
+		btn3.clicked.connect(chrome_open)
+		btn3.setFixedWidth(300)
+		btn3.setFixedHeight(100)
+		btn3.setStyleSheet("QPushButton { color: rgb(0,0,0);\
+            font-size: 18pt;border: ;background-image: url('background1.jpg');\
             border-style: outset;border-width: 2px;border-radius: 10px;\
             border-color: white;padding: 10px;}");
 		btn3.resize(btn3.minimumSizeHint())
 		btn3.move(4,100)
 
 		label = QtGui.QLabel("Enter Image URL: ")
-		url = QtGui.QLineEdit()
+		label.setStyleSheet("QLabel {color: rgb(0,0,0); font-size: 18pt;background-image: url('background1.jpg');\
+            border-style: outset;border-width: 2px;border-radius: 10px;\
+            border-color: white;padding: 10px;}")
+		self.url = QtGui.QLineEdit()
 
-		name = url.text()
+		name = self.url.text()
 		print (name)
 		btnok = QtGui.QPushButton("Ok",self)
 		btnok.clicked.connect(self.url_image_save)
 
 		boxurl = QtGui.QHBoxLayout()
 		boxurl.addWidget(label)
-		boxurl.addWidget(url)
+		boxurl.addWidget(self.url)
 		boxurl.addWidget(btnok)
 		
+		'''
 		btn4 = QtGui.QPushButton("Drag and Drop", self)
-		image = btn4.clicked.connect(drag_drop)
+		btn4.clicked.connect(drag_drop)
 		btn4.setStyleSheet("QPushButton { color: rgb(255, 255, 255);\
             font-size: 16pt;border: ;background-color: rgb(50,80,109);\
             border-style: outset;border-width: 2px;border-radius: 10px;\
             border-color: white;padding: 10px;}");
 		btn4.resize(btn4.minimumSizeHint())
 		btn4.move(4,150)
-		
+		'''
+
 		vbox1 = QtGui.QVBoxLayout()
-		vbox1.addWidget(btn1)
-		vbox1.addWidget(btn2)
-		vbox1.addWidget(btn3)
-		vbox1.addLayout(boxurl)
-		vbox1.addWidget(btn4)
+		vbox1.addWidget(title,1)
+		vbox1.addWidget(btn1,2)
+		vbox1.addWidget(btn2,2)
+		vbox1.addWidget(btn3,2)
+		vbox1.addLayout(boxurl,1)
+		#vbox1.addWidget(btn4)
 
 		layout.addLayout(vbox1)
 
@@ -404,8 +480,20 @@ class LabelImageChildWindow(QtGui.QWidget):
 		mainLayout.addWidget(self.parameterBox,1)
 		mainLayout.addWidget(buttonBox,1)
 		self.setLayout(mainLayout)
+		
+
+		#self.setStyleSheet("background-image: url('background10.jpg');")
+		#btn1.setStyleSheet(" { background-color: rgb(58,83,155); }")
+		
+		p = QtGui.QPalette()
+		brush = QtGui.QBrush(QtCore.Qt.white,QtGui.QPixmap('background19.jpg'))
+		p.setBrush(QtGui.QPalette.Active,QtGui.QPalette.Window,brush)
+		p.setBrush(QtGui.QPalette.Inactive,QtGui.QPalette.Window,brush)
+		p.setBrush(QtGui.QPalette.Disabled,QtGui.QPalette.Window,brush)
+		self.setPalette(p)
+		
 		#self.setStyleSheet("background-color: rgb(255,255,255);")
-		self.setGeometry(1, 1, 1500,1200)
+		self.setGeometry(1, 1, 2000, 1800)
 		#self.move(300,10)
 		self.setWindowTitle("Visual Recognition using CNN")
 		self.show()
@@ -414,9 +502,10 @@ class LabelImageChildWindow(QtGui.QWidget):
 		self.labelimage = QtGui.QGroupBox()
 		hlayout = QtGui.QHBoxLayout()
 		pixmap = QtGui.QPixmap("sample.jpg")
+		newPix = pixmap.scaled(300, 700, QtCore.Qt.KeepAspectRatio)
 		#pixmap_resized = pixmap.scaled(720, 405, QtCore.Qt.KeepAspectRatio)
 		lbl = QtGui.QLabel(self)
-		lbl.setPixmap(pixmap)
+		lbl.setPixmap(newPix)
 		lbl.setAlignment(QtCore.Qt.AlignCenter)
 		#lbl.setStyleSheet("QLabel {background-color: red;}")
 		#lbl.resize(700, 900)
@@ -458,39 +547,39 @@ class LabelImageChildWindow(QtGui.QWidget):
 		label4 = QtGui.QLabel()
 		label5 = QtGui.QLabel()
 
-		#label1.setText("1")
-		label1.setText(result[0][0][1])
+		label1.setText("1")
+		#label1.setText(result[0][0][1])
 		label1.setAlignment(QtCore.Qt.AlignCenter)
-		label1.setStyleSheet("QLabel { color: white;\
-            font-size: 16pt; background-color: rgb(77, 194, 71);\
+		label1.setStyleSheet("QLabel { color: rgb(77, 194, 71);\
+            font-size: 16pt; background-color: white;\
             border-style: outset;border-radius: 7px;border-color: white;}")
 
-		#label2.setText("2")
-		label2.setText(result[0][0][1])
+		label2.setText("2")
+		#label2.setText(result[0][0][1])
 		label2.setAlignment(QtCore.Qt.AlignCenter)
-		label2.setStyleSheet("QLabel { color: white;\
-            font-size: 16pt; background-color: rgb(77, 194, 71);\
+		label2.setStyleSheet("QLabel { color: rgb(77, 194, 71);\
+            font-size: 16pt; background-color: white;\
             border-style: outset;border-radius: 7px;border-color: white;}")
 
-		#label3.setText("3")
-		label3.setText(result[0][0][1])
+		label3.setText("3")
+		#label3.setText(result[0][0][1])
 		label3.setAlignment(QtCore.Qt.AlignCenter)
-		label3.setStyleSheet("QLabel { color: white;\
-            font-size: 16pt; background-color: rgb(77, 194, 71);\
+		label3.setStyleSheet("QLabel { color: rgb(77, 194, 71);\
+            font-size: 16pt; background-color: white;\
             border-style: outset;border-radius: 7px;border-color: white;}")
 
-		#label4.setText("4")
-		label4.setText(result[0][0][1])
+		label4.setText("4")
+		#label4.setText(result[0][0][1])
 		label4.setAlignment(QtCore.Qt.AlignCenter)
-		label4.setStyleSheet("QLabel { color: white;\
-            font-size: 16pt; background-color: rgb(77, 194, 71);\
+		label4.setStyleSheet("QLabel { color: rgb(77, 194, 71);\
+            font-size: 16pt; background-color: white;\
             border-style: outset;border-radius: 7px;border-color: white;}")
 
-		#label5.setText("5")
-		label5.setText(result[0][0][1])
+		label5.setText("5")
+		#label5.setText(result[0][0][1])
 		label5.setAlignment(QtCore.Qt.AlignCenter)
-		label5.setStyleSheet("QLabel { color: white;\
-            font-size: 16pt; background-color: rgb(77, 194, 71);\
+		label5.setStyleSheet("QLabel { color: rgb(77, 194, 71);\
+            font-size: 16pt; background-color: white;\
             border-style: outset;border-radius: 7px;border-color: white;}")
 
 		label1.setFixedSize(200,60)
@@ -534,7 +623,7 @@ class LabelImageChildWindow(QtGui.QWidget):
 
 	def createParameterBox(self):
 		self.parameterBox = QtGui.QGroupBox()
-		layout = QtGui.QHBoxLayout()
+		layout1 = QtGui.QHBoxLayout()
 
 		label1 = QtGui.QLabel()
 		label2 = QtGui.QLabel()
@@ -564,11 +653,51 @@ class LabelImageChildWindow(QtGui.QWidget):
 		label4.setStyleSheet("QLabel { color: rgb(77, 194, 71);\
             font-size: 16pt; background-color: white;\
             border-style: outset;border-radius: 5px;border-color: white;}");
- 
-		layout.addWidget(label1)
-		layout.addWidget(label2)
-		layout.addWidget(label3)
-		layout.addWidget(label4)
+
+		layout1.addWidget(label1)
+		layout1.addWidget(label2)
+		layout1.addWidget(label3)
+		layout1.addWidget(label4)
+
+		layout2 = QtGui.QHBoxLayout()
+
+		label11 = QtGui.QLabel()
+		label22 = QtGui.QLabel()
+		label33 = QtGui.QLabel()
+		label44 = QtGui.QLabel()
+
+		label11.setText("Error Rate")
+		label11.setAlignment(QtCore.Qt.AlignCenter)
+		label11.setStyleSheet("QLabel { color: rgb(77, 194, 71);\
+            font-size: 16pt; background-color: white;\
+            border-style: outset;border-radius: 5px;border-color: white;}");
+		
+		label22.setText("Momemtum")
+		label22.setAlignment(QtCore.Qt.AlignCenter)
+		label22.setStyleSheet("QLabel { color: rgb(77, 194, 71);\
+            font-size: 16pt; background-color: white;\
+            border-style: outset;border-radius: 5px;border-color: white;}");
+		
+		label33.setText("Mini Batch Size")
+		label33.setAlignment(QtCore.Qt.AlignCenter)
+		label33.setStyleSheet("QLabel { color: rgb(77, 194, 71);\
+            font-size: 16pt; background-color: white;\
+            border-style: outset;border-radius: 5px;border-color: white;}");
+		
+		label44.setText("Training Iterations")
+		label44.setAlignment(QtCore.Qt.AlignCenter)
+		label44.setStyleSheet("QLabel { color: rgb(77, 194, 71);\
+            font-size: 16pt; background-color: white;\
+            border-style: outset;border-radius: 5px;border-color: white;}");
+
+		layout2.addWidget(label11)
+		layout2.addWidget(label22)
+		layout2.addWidget(label33)
+		layout2.addWidget(label44)
+
+		layout = QtGui.QVBoxLayout()
+		layout.addLayout(layout1)
+		layout.addLayout(layout2)
 
 		self.parameterBox.setLayout(layout)
 
